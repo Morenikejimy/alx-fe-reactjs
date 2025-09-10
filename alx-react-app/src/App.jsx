e65b29f6d63faca13cd904be5955204262c455c1
Morenikejimy <amorenikeji198@gmail.com>
2025-09-10 07:02:28 -0700

    Add userProfile.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -4,18 +4,20 @@ import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
 
 function App() {
-
+  
   return (
     <div>
       <Header />
-      <MainContent />
 
-        <UserProfile 
+    <MainContent />
+
+    {/*Add your UserProfile here */}
+      <UserProfile 
         name="Alice" 
         age={25} 
         bio="Loves hiking and photography" 
       />
-      
+
       <Footer />
     </div>
   );
