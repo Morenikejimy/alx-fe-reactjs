c2eb2d87691dfa81a6e815ad4faa44fe4483eed4
Morenikejimy <amorenikeji198@gmail.com>
2025-09-10 07:06:15 -0700

    Welcome message in App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,7 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
+import WelcomeMessage from './components/WelcomeMessage';
 
 function App() {
   
